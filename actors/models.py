2645e7f63fb7b6918e17bc1c452f29d0b0ec0521
Giovanni Pastrano Nuñez <giovanni.pastrano1@hotmail.com>
2019-08-12 22:42:59 -0400

    Modelado hasta el momento del db y los serializers correspondientes.

--- actors/models.py
+++ actors/models.py
@@ -2,9 +2,104 @@
 from django.db import models
 
 # Create your models here.
-class Actor(models.Model):
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    dp = models.CharField(max_length=512, blank=True, null=True)
-    age = models.IntegerField(blank=True, null=True)
+class Personas(models.Model):
+    ID = models.IntegerField(default = 1,primary_key = True)
+    PRIMERNOMBRE = models.CharField(max_length = 20)
+    SEGUNDONOMBRE = models.CharField(max_length = 20)
+    APELLIDOS = models.CharField(max_length = 40)
+    CEDULA = models.CharField(max_length = 15)
+    TELEFONO = models.CharField(max_length = 25)
+    DIRECCION = models.CharField(max_length = 25)
+    GENERO = models.CharField(max_length = 25)
+    FECHANACIMIENTO = models.DateField()
+    CORREO = models.EmailField(max_length = 100, unique = True)
+    VERIFICACIONCORREO = models.BooleanField(default = False)
+    FOTOPERFIL = models.CharField(max_length = 25)
 
+class TiposAutenticaciones(models.Model):
+    TIPO = models.CharField(max_length = 20)
+
+class Usuarios(models.Model):
+    PERSONA = models.ForeignKey(Personas, on_delete = models.CASCADE)
+    AUTENTICACION = models.ForeignKey(TiposAutenticaciones, on_delete = models.CASCADE)
+    USERNAME = models.CharField(max_length = 25)
+    PASSWORD = models.CharField(max_length = 25)
+    FECHACREACION = models.DateField()
+    ESTADO = models.BooleanField(default = False)
+
+
+class AuditoriaUsuarios(models.Model):
+    USUARIO = models.ForeignKey(Usuarios, on_delete = models.CASCADE)
+    ULTIMASESION = models.DateField()
+    FECHAAUDITORIA = models.DateField()
+    ESTATUS = models.CharField(max_length = 25)
+    ACCION = models.CharField(max_length = 25)
+
+class Promocion(models.Model):
+    FECHACREACION = models.DateField()
+    DESCRIPCION = models.CharField(max_length = 250)
+    ESTATUS = models.BooleanField(default = False)
+    FECHAVIGENCIA = models.DateField()
+
+class Recetas(models.Model):
+    SEGUNDONOMBRE = models.ForeignKey(Promocion, on_delete = models.CASCADE)
+    FOTORECETA = models.CharField(max_length = 100)
+    FECHAFOTO = models.DateField()
+    
+class Estados(models.Model):
+    NOMBRE = models.CharField(max_length = 50)
+    TIPO = models.CharField(max_length = 50)
+    DESCRIPCION = models.CharField(max_length = 50)
+
+class Productos(models.Model):
+    DESCRIPCION = models.CharField(max_length = 75)
+
+class DetallePromocion(models.Model):
+    PROMOCION = models.ForeignKey(Promocion, on_delete = models.CASCADE)
+    PRODUCTO = models.ForeignKey(Productos, on_delete = models.CASCADE)
+
+class Farmacias(models.Model):
+    NOMBRE = models.CharField(max_length = 100)
+    DIRECCION = models.CharField(max_length = 100)
+
+class Bancos(models.Model):
+    NOMBRE = models.CharField(max_length = 50)
+    DIRECCION = models.CharField(max_length = 50)
+    TELEFONO = models.CharField(max_length = 50)
+
+class TipoNC(models.Model):
+    TIPO = models.CharField(max_length = 50)
+    DESCRIPCION = models.CharField(max_length = 50)
+
+class NotasdeCredito(models.Model):
+    PROMOCION = models.OneToOneField(Promocion, on_delete = models.CASCADE)
+    BANCO = models.OneToOneField(Bancos, on_delete = models.CASCADE)
+    FARMACIA = models.OneToOneField(Farmacias, on_delete = models.CASCADE)
+    TIPONC = models.OneToOneField(TipoNC, on_delete = models.CASCADE)
+    FECHANC = models.DateField()
+    MONTO = models.IntegerField()
+
+class Cupones(models.Model):
+    PERSONA = models.OneToOneField(Personas, on_delete = models.CASCADE)
+    ESTADO = models.OneToOneField(Estados, on_delete = models.CASCADE)
+    RECETA = models.OneToOneField(Recetas, on_delete = models.CASCADE)
+    FARMACIA = models.OneToOneField(Farmacias, on_delete = models.CASCADE)
+    NOTACREDITO = models.ForeignKey(NotasdeCredito, on_delete = models.CASCADE)
+    FECHACUPON = models.DateField()
+    DESCRIPCION = models.CharField(max_length = 100)
+
+class DetalleCupon(models.Model):
+    PRODUCTO = models.ForeignKey(Productos, on_delete = models.CASCADE)
+    DESCUENTO = models.IntegerField()
+
+class AuditoriaCupon(models.Model):
+    CUPON = models.ForeignKey(Cupones, on_delete = models.CASCADE)
+    ESTADO = models.OneToOneField(Estados, on_delete = models.CASCADE)
+    USUARIO = models.OneToOneField(Usuarios, on_delete = models.CASCADE)
+    FECHAESTADO = models.DateField()
+    DESCRIPCION = models.CharField(max_length = 50)
+
+class Inventario(models.Model):
+    PROMOCION = models.OneToOneField(Promocion, on_delete = models.CASCADE)
+    PRODUCTO = models.OneToOneField(Productos, on_delete = models.CASCADE)
+    CANTIDAD = models.IntegerField()
--- actors/serializers.py
+++ actors/serializers.py
@@ -1,8 +1,77 @@
 
 from rest_framework.serializers import ModelSerializer
-from .models import Actor
+from .models import *
 
-class ActorSerializer(ModelSerializer):
+class PersonasSerializer(ModelSerializer):
     class Meta:
-        model = Actor
+        model = Personas
+        fields = '__all__'
+
+class TiposAutenticacionesSerializer(ModelSerializer):
+    class Meta:
+        model = TiposAutenticaciones
+        fields = '__all__'
+
+class AuditoriaUsuariosSerializer(ModelSerializer):
+    class Meta:
+        model = AuditoriaUsuarios
+        fields = '__all__'   
+
+class RecetasSerializer(ModelSerializer):
+    class Meta:
+        model = Recetas
+        fields = '__all__'
+
+class EstadosSerializer(ModelSerializer):
+    class Meta:
+        model = Estados
+        fields = '__all__'
+
+class ProductosSerializer(ModelSerializer):
+    class Meta:
+        model = Productos
+        fields = '__all__'        
+class DetallePromocionSerializer(ModelSerializer):
+    class Meta:
+        model = DetallePromocion
+        fields = '__all__'
+
+class FarmaciasSerializer(ModelSerializer):
+    class Meta:
+        model = Farmacias
+        fields = '__all__'
+
+class BancosSerializer(ModelSerializer):
+    class Meta:
+        model = Bancos
+        fields = '__all__'
+
+class TipoNCSerializer(ModelSerializer):
+    class Meta:
+        model = TipoNC
+        fields = '__all__'
+
+class NotasdeCreditoSerializer(ModelSerializer):
+    class Meta:
+        model = NotasdeCredito
+        fields = '__all__'
+
+class CuponesSerializer(ModelSerializer):
+    class Meta:
+        model = Cupones
+        fields = '__all__'
+
+class DetalleCuponSerializer(ModelSerializer):
+    class Meta:
+        model = DetalleCupon
+        fields = '__all__'
+
+class AuditoriaCuponSerializer(ModelSerializer):
+    class Meta:
+        model = AuditoriaCupon
+        fields = '__all__'
+
+class InventarioSerializer(ModelSerializer):
+    class Meta:
+        model = Inventario
         fields = '__all__'
